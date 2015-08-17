0070a4b56480a5c6c2cc2e6bb71f77f1613a9614
nodejh <replyjh@gmail.com>
2015-08-17 21:39:49 +0800

    modified:   .gitignore 	modified:   README.md 	modified:   doc/assessment.sql
    add field `free_time` to `table.Teacher`,this is used to show the free time of a teacher
    
    chage the type of `type` field to `varchar` on `table.Teacher`. It will be more clear to show the consulting service of a tearcher.

--- doc/assessment.sql
+++ doc/assessment.sql
@@ -17,10 +17,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `Admin` (
-  `admin_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `admin_id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `admin_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `type` tinyint(1) NOT NULL COMMENT '管理员类型，用来分配权限'
+  `type` tinyINT(1) NOT NULL COMMENT '管理员类型，用来分配权限,1为超级管理员，2为普通管理员'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='管理员';
 
 -- --------------------------------------------------------
@@ -30,10 +30,10 @@ CREATE TABLE IF NOT EXISTS `Admin` (
 --
 
 CREATE TABLE IF NOT EXISTS `Card` (
-  `card_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `card_id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `type` tinyint(1) NOT NULL COMMENT '会员卡类型'
+  `type` tinyINT(1) NOT NULL COMMENT '会员卡类型。1可以做两个测评，可以看到简单结果，不能预约咨询师；2可以做四个测评，能预约'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='会员卡';
 
 -- --------------------------------------------------------
@@ -43,9 +43,11 @@ CREATE TABLE IF NOT EXISTS `Card` (
 --
 
 CREATE TABLE IF NOT EXISTS `Comment` (
-  `coment_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `parent_id` int(11) DEFAULT NULL,
-  `score` int(11) NOT NULL,
+  `coment_id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` INT(11) NOT NULL,
+  `teacher_id` INT(11)  NOT NULL,
+  `attidude_score` INT(11) NOT NULL COMMENT '服务态度评分(1-5)',
+  `professional_score` INT(11) NOT NULL COMMENT '专业程度评分(1-5)',
   `content` varchar(3000) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='评论';
 
@@ -56,12 +58,16 @@ CREATE TABLE IF NOT EXISTS `Comment` (
 --
 
 CREATE TABLE IF NOT EXISTS `Teacher` (
-  `teacher_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `teacher_id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `password` int(200) NOT NULL,
+  `password` INT(200) NOT NULL,
+  `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `picture` varchar(200) COLLATE utf8_unicode_ci NOT NULL COMMENT '咨询师图片',
-  `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL
+  `gender` tinyINT(1) NOT NULL,
+  `email` varchar(200) COLLATE utf8_unicode_ci NULL,
+  `type` VARCHAR(15) NOT NULL COMMENT '[1,2,3,4,5,6] 多选，用逗号隔开',
+  `free_time` TINYINT(2) NOT NULL COMMENT '空闲时间，两位整数，星期+时间段。12 周一，第二个时段。时段：9:00-10:30-12:00;2:30-4:00-5:30;7:00-8:30-10:00',
+  `introduction` varchar(255) NOT NULL COMMENT '咨询师简介'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='咨询师';
 
 -- --------------------------------------------------------
@@ -71,13 +77,15 @@ CREATE TABLE IF NOT EXISTS `Teacher` (
 --
 
 CREATE TABLE IF NOT EXISTS `User` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `gender` tinyint(1) NOT NULL,
-  `subject` tinyint(2) NOT NULL,
-  `senior_high` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `university` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+  `email` varchar(200) COLLATE utf8_unicode_ci NULL,
+  `gender` tinyINT(1) NOT NULL,
+  `study_status` TINYINT(1) NOT NULL COMMENT '1高中 2大学及以上,3工作未满1年,4工作1-3年,5工作3-5年,6工作5年以上',
+  `school_name` VARCHAR(200) COLLATE utf8_unicode_ci NOT NULL,
+  `college` VARCHAR(200) COLLATE utf8_unicode_ci NULL COMMENT '专业（2）',
+  `senior_high_status` INT(4) NULL COMMENT '艺体生，少数名族考生，是否国家专项计划，是否军校或国防生（1） ［0000］0表示否，1表示是',
+  `card_id` INT(11) NOT NULL,
+  `type` TINYINT(1) NOT NULL COMMENT '1可以做两个测评，可以看到简单结果，不能预约咨询师；2可以做四个测评，能预约'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='来访者';
-
